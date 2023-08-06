525d61b6e9295f03801942cae5170ab4f9a24191
ian99165 <opportunit26@gmail.com>
2023-08-06 21:46:14 +0800

    OAO

--- index.js
+++ index.js
@@ -5,22 +5,11 @@ var path = require('path');
 var compression = require('compression');
 
 //Compress all requests
-app.use(compression());
 
 //Serve static files
 app.use('/', express.static(path.resolve(__dirname , './GMAEWAL')));
 
 //Serve the compressed version of Unity files
-app.use('/assets/vendors/unity/Release/:filename', function(req, res){
-    var extensionFile = path.extname(req.params.filename);
-    if(extensionFile === '.data' || extensionFile === '.mem'){
-        res.header('Content-Type', 'application/octet-stream');
-    }
-    //Response used gzip encoding
-    res.header('Content-Encoding', 'gzip');
-    //Send file if there is a match into "Compressed" Unity folder
-    res.sendFile(path.resolve(__dirname , '../public/vendors/unity/Compressed/'+req.params.filename+'gz'));
-});
 
 app.listen(80, () => {
   console.log('Server has started!')
